d56c36b03e28558e4396b27a313b503d82746767
Leandro <leandro@otimaideia.com.br>
2017-10-09 17:49:53 -0300

    Criação do Modelo de Cerveja para realização de teste de rotas e envio de objetos via formulário POST

--- src/main/java/org/manzatech/brewer/controller/CervejasController.java
+++ src/main/java/org/manzatech/brewer/controller/CervejasController.java
@@ -2,12 +2,19 @@ package org.manzatech.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CervejasController {
 
     @GetMapping("/cervejas/novo")
     public String novo(){
+        return "cerveja/CadastroCerveja";
+    }
+
+    @PostMapping("/cervejas/novo")
+    public String cadastrar(){
+        System.out.print(">>>>>>> cadastrar");
         return "cerveja/CadastroCerveja";
     }
 }
